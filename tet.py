1a3f444acdaf1f0c8487dc58492497382ac782b2
ahmed2005th <96220682+ahmed2005th@users.noreply.github.com>
2022-01-02 23:46:54 +0200

    Add files via upload

---
+++ tet.py
@@ -0,0 +1,25 @@
+import os
+os.system ("clear")
+##############################
+n = input ("Enter Your username :")
+if n == "rd" :
+    print ("Welcome Mr :"+n)
+else:
+    r = input ("Please Try Agin >>> :")
+    if r == "rd" :
+        print ("Good Mr :"+r)
+    else:
+        print("Sory I dont You Can..! Mr :"+n)
+##############################################
+p = input ("Enter The Password : ")
+if p == "123" :
+    print ("Exlant Mr : "+r)
+    print ("Thank You For Sgin In Mr : "+n)
+else:
+     t = input ("Please Try Agin >>> :")
+     if t == "123" :
+          print("Exlant Work :")
+          print ("Thank You For Sgin In Mr : "+r)
+     else:
+          print("I Dont You Can Mr : "+n)
+                
